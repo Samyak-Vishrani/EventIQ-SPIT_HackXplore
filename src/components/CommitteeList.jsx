989f073c04074c6f1d7e95d7434ac603ee825d9b
Smit Pawar <dynaster21@gmail.com>
2025-03-29 14:23:13 +0530

    committee list integrated

--- src/components/CommitteeList.jsx
+++ src/components/CommitteeList.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/CommitteeList.css";
 import Modal from "./Model";
+import url from "../apis/urls"
 
 const CommitteeList = () => {
-    const [committees] = useState(["Unicode", "Synapse", "ACM"]);
+    const [committees, setCommittees] = useState([]);
     const [selectedCommittee, setSelectedCommittee] = useState(null);
+
+    useEffect(() => {
+        fetch(`${url}/committee`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            setCommittees(data);
+        })
+        .catch(error => console.error("Error fetching committees:", error));
+    }, []);
 
     return (
         <div className="committee-list-page">
             <h1>Committee Lists</h1>
-            {committees.map((com, index) => (
-                <div key={index} className="committee-box">
-                    <h1>{com}</h1>
-                    <button onClick={() => setSelectedCommittee(com)}>Join</button>
+            {committees.map((committee, index) => (
+                <div key={committee._id || index} className="committee-box">
+                    <h1>{committee.name}</h1>
+                    <button onClick={() => setSelectedCommittee(committee)}>Join</button>
                 </div>
             ))}
 
